94704b2a946f2299e52007b5440cfefbd3061ba8
Corey Prater <bp01442@gmail.com>
2026-01-26 17:09:01 -0500

    remove keys

--- frontend/chat/js/Backup-Working-1-24/supabase.js
+++
@@ -1,7 +0,0 @@
-// PLACE IN /js/supabase.js
-/*import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
-
-export const supabase = createClient(
-  'https://fhtzawegbejlfyqkipnp.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZodHphd2VnYmVqbGZ5cWtpcG5wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg0Mzk4MDMsImV4cCI6MjA4NDAxNTgwM30.13S_lHY3ftr98r3lPvAGLKOrfyVI2hTFfywLTjwUmug'
-);
-*/
